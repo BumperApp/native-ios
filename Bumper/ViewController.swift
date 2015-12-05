43e667b7333d47674a34ad503ed73ee3e1d6492c
Connor Hendicott <hello@connorhendicott.com>
2015-12-05 17:25:45 +1000

    Initial Commit

---
+++ Bumper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bumper
+//
+//  Created by Connor Hendicott on 5/12/2015.
+//  Copyright © 2015 Connor Hendicott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BumperTests/BumperTests.swift
@@ -0,0 +1,36 @@
+//
+//  BumperTests.swift
+//  BumperTests
+//
+//  Created by Connor Hendicott on 5/12/2015.
+//  Copyright © 2015 Connor Hendicott. All rights reserved.
+//
+
+import XCTest
+@testable import Bumper
+
+class BumperTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
